6b95d51ffc0bc9dec6ed92e7be5ffbc38f946cdb
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-27 20:30:56 +0000

    march 27th

--- src/screens/Crime/components/CrimePost.tsx
+++ src/screens/Crime/components/CrimePost.tsx
@@ -8,6 +8,14 @@ function CrimePost() {
                 <p>Mon 12 Jan 2022</p> <div className='cph_s' />
                 <p>6:25pm</p>
             </div>
+            <div className='CrimePost_container'>
+                <p>A truck full of cocaine was just comprehended along badagry express way by 5:00 pm. The suspect fled the scene leaving his truck full of cocaine. If you have any ...</p>
+            </div>
+            <div className='CrimePost_bottom'>
+                <div className='readMore_button'>
+                    <p>Read more</p>
+                </div>
+            </div>
         </div>
     );
 }
