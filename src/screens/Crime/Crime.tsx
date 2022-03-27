32458ec56af2895ff2b6f390aaf6495502a6f347
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-27 19:34:31 +0000

    march 27th

--- src/screens/Crime/Crime.tsx
+++ src/screens/Crime/Crime.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import './Crime.css';
 
 import { Header } from '../../components';
+import Form from './components/Form';
 
 function Crime() {
     return (
         <div className='CrimePage'>
             <Header />
-            
+            <section>
+                <div className='cp_d1' />
+                <div className='cp_d2'>
+                    <Form />
+                </div>
+            </section>
         </div>
     );
 }
---
+++ src/screens/Crime/components/Form.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Form() {
+    return (
+        <div className='CrimePage_Form'>
+            <h3 className='cp_title'>Post a <span>crime update</span></h3>
+        </div>
+    );
+}
+
+export default Form;
