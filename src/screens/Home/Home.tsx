83dfda79940dda429a4b10952fe3ac036c8d8a1a
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-28 02:29:16 +0000

    march 28th

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -4,9 +4,18 @@ import Lottie from 'react-lottie';
 import { Header } from '../../components';
 
 const background = require('../../assets/background.png');
+const animationData = require('../../assets/safe-city.json');
 
 function Home() {
-    console.log('Home page')
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+      
     return (
         <div className='HomePage' style={{backgroundImage: `url(${background})`}}>
             <Header />
@@ -14,6 +23,9 @@ function Home() {
                 <div className='SD1'>
                     <p className='sd_header'>Helping Nigerian citizens stay informed and <span>safe</span> </p>
                     <p className='sd_main'>Connect and live more safely. Safe is a personal safety network that empowers you to protect yourself and the people and places you care about.</p>
+                </div>
+                <div className='SD2'>
+                    <Lottie options={defaultOptions} height={500} width={500} />
                 </div>
             </section>
         </div>
