27999ae5d3ee2a19d0317c59e8fb241ebeec4108
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-29 19:12:22 +0000

    march 29th

--- src/screens/Crime/components/Success.tsx
+++ src/screens/Crime/components/Success.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { MdCheckCircle, MdCheckCircleOutline } from 'react-icons/md';
 
 function Success() {
     return (
         <div className='PostSuccess'>
-            
+            <MdCheckCircleOutline size={40} color='#FF2365' />
+            <p className='postSuccess_title'>Post successful</p>
+            <p className='postSuccess_extra'>please reload page</p>
         </div>
     );
 }
