88441097aa7f01c5c67990969a6f1c2030560f69
Ahumareze <ahumarezeifeanyi001@gmail.com>
2022-03-31 02:05:53 +0000

    march 31th

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -23,6 +23,15 @@ function Home(props: any) {
                 <div className='SD1'>
                     <p className='sd_header'>Helping Nigerian citizens stay informed and <span>safe</span> </p>
                     <p className='sd_main'>Connect and live more safely. Safe is a personal safety network that empowers you to protect yourself and the people and places you care about.</p>
+                    <div className='sd_box'>
+                        <div />
+                        <div className='sd_details'>
+                            <p className='sd_award_title'>Vanguard App of the year</p>
+                            <div className='sd_award_main'>
+                                <p><span>"</span>Safe won the vanguard best web application 2021. Getting the support from the chief of police, saying that safe was going to increase the number of awareness amongst citizens<span>"</span></p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className='SD2'>
                     <Lottie options={defaultOptions} height={500} width={500} />
